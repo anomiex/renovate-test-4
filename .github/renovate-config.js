ebbc5adf5d71ee606e189ba62acf38725ff817e7
Brad Jorsch <anomie@users.sourceforge.net>
2026-01-22 11:12:03 -0500

    Update for new bug

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,21 +1,6 @@
 module.exports = {
-	branchPrefix: 'renovate-sh/',
+	branchPrefix: 'renovate/',
 	gitAuthor: 'Renovate Bot (self-hosted) <bot@renovateapp.com>',
 	platform: 'github',
 	repositories: [ 'anomiex/renovate-test-4' ],
-
-	// We're including configuration in this file.
-	onboarding: false,
-	requireConfig: false,
-
-	// Post-upgrade command
-	allowedCommands: [ '.github/files/renovate-post-upgrade-run.sh' ],
-	postUpgradeTasks: {
-		commands: [ '.github/files/renovate-post-upgrade-run.sh {{{branchName}}}' ],
-		executionMode: 'branch',
-	},
-
-	// This is the renovate configuration.
-	extends: [ 'config:recommended' ],
-	timezone: 'UTC',
 };
